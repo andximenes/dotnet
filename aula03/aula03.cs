c5ad08081b9fa8a89a1b850b8caa3de8a5670247
André Ximenes <andre1ximenes92@gmail.com>
2023-10-25 12:05:04 -0300

    C# aula 03 - Tipos

---
+++ aula03/aula03.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Aula03 {
+    static void Main(string[] args){
+        int num = 2;
+        char letra = 'a';
+        float valor = 1.5f;
+        string nome = "João";
+
+        Console.Write(nome);
+    }
+}
