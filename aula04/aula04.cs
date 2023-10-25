765fa73a64a67e6c27cd855985bc0e0bb119c617
André Ximenes <andre1ximenes92@gmail.com>
2023-10-25 12:50:04 -0300

    c# aula 04 - Variável global x Local

---
+++ aula04/aula04.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Aula04{
+    
+    //Variável global
+    int num1 = 5;
+    static void Main(string[] args){
+        //Variavel local
+        int num2 = 10;
+    }
+   
+}
