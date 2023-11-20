07232a10f62b8330cdd1465bca5e79d2ba398960
André Ximenes <andre1ximenes92@gmail.com>
2023-11-20 16:35:09 -0300

    Aula 44 c# - Struct

---
+++ aula44/Structs/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Teste
+{
+    static void Main()
+    {
+        Carro carro = new Carro("WV", "Golf", "Vermelho");
+        
+        carro.info();
+
+    }
+}
+
+//ver aula 44 sobre struct 
---
+++ aula44/Structs/struct.cs
@@ -0,0 +1,24 @@
+using System;
+
+struct Carro
+{
+    public string marca;
+    public string modelo;
+    public string cor;
+
+    public Carro(string marca, string modelo, string cor)
+    {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.cor = cor;
+    }
+
+    public void info()
+    {   
+     
+        Console.WriteLine($"Marca: {this.marca}");
+        Console.WriteLine($"Modelo: {this.modelo}");
+        Console.WriteLine($"Cor: {this.cor}");
+        
+    }
+}
